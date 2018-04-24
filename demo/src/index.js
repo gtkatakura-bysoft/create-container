e9b24baeb8864ef6b3ce526ff13819c01489a61f
Héliton Nordt <hnordt@hnordt.com>
2018-04-24 21:40:27 +0100

    Initial commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,60 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from "react"
 
-import Example from '../../src'
+import createContainer from "../../src"
+import { render } from "react-dom"
 
-class Demo extends Component {
-  render() {
-    return <div>
-      <h1>create-container Demo</h1>
-      <Example/>
-    </div>
+const CounterContainer = createContainer(
+  { count: 0 },
+  {
+    INCREMENT: state => ({ count: state.count + 1 }),
+    DECREMENT: state => ({ count: state.count - 1 }),
+    INCREMENT_BY: (state, action) => ({ count: state.count + action.by })
+  },
+  {
+    increment: () => ({ type: "INCREMENT" }),
+    decrement: () => ({ type: "DECREMENT" }),
+    incrementBy: by => ({ type: "INCREMENT_BY", by }),
+
+    incrementByAsync: by => ({ incrementBy }) =>
+      setTimeout(() => incrementBy(by), 1000)
   }
-}
+)
 
-render(<Demo/>, document.querySelector('#demo'))
+const Demo = () => (
+  <CounterContainer.Provider>
+    <p>
+      Current count: <Count />
+    </p>
+    <IncrementButton />
+    <DecrementButton />
+    <AsyncIncrementButton by={5} />
+  </CounterContainer.Provider>
+)
+
+const Count = () => (
+  <CounterContainer.Consumer>{({ count }) => count}</CounterContainer.Consumer>
+)
+
+const IncrementButton = () => (
+  <CounterContainer.Consumer>
+    {({ increment }) => <button onClick={increment}>Increment</button>}
+  </CounterContainer.Consumer>
+)
+
+const DecrementButton = () => (
+  <CounterContainer.Consumer>
+    {({ decrement }) => <button onClick={decrement}>Decrement</button>}
+  </CounterContainer.Consumer>
+)
+
+const AsyncIncrementButton = ({ by }) => (
+  <CounterContainer.Consumer>
+    {({ incrementByAsync }) => (
+      <button onClick={() => incrementByAsync(by)}>
+        Increment by {by} (takes 1s)
+      </button>
+    )}
+  </CounterContainer.Consumer>
+)
+
+render(<Demo />, document.querySelector("#demo"))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,63 @@
-import React, {Component} from 'react'
+import React, { Component } from "react"
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+const createContainer = (
+  initialState = {},
+  reducer = state => state,
+  actions = {}
+) => {
+  const enhancedReducer = (state = initialState, action) =>
+    typeof reducer === "object"
+      ? reducer[action.type]
+        ? reducer[action.type](state, action)
+        : state
+      : reducer(state, action)
+
+  const Context = React.createContext({
+    ...enhancedReducer(undefined, {}),
+    ...Object.keys(actions).reduce(
+      (result, action) => ({
+        ...result,
+        [action]: () => {}
+      }),
+      {}
+    )
+  })
+
+  class Provider extends Component {
+    constructor(props) {
+      super(props)
+
+      this.actions = Object.keys(actions).reduce(
+        (result, action) => ({
+          ...result,
+          [action]: (...args) => this.dispatch(actions[action](...args))
+        }),
+        {}
+      )
+
+      this.state = {
+        ...enhancedReducer(undefined, {}),
+        ...this.actions
+      }
+    }
+
+    dispatch = action =>
+      typeof action === "function"
+        ? action({ ...this.state, ...this.actions })
+        : new Promise(resolve =>
+            this.setState(state => enhancedReducer(state, action), resolve)
+          )
+
+    render() {
+      return (
+        <Context.Provider value={this.state}>
+          {this.props.children}
+        </Context.Provider>
+      )
+    }
   }
+
+  return { Provider, Consumer: Context.Consumer }
 }
+
+export default createContainer
